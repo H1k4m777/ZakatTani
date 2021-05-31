1838f10e4fc8aed3cb385c9f7b7b046672b8f187
H1k4m777 <63985565+H1k4m777@users.noreply.github.com>
2021-05-31 18:58:53 +0700

    update code #1
    Co-Authored-By: H1k4m777 <63985565+H1k4m777@users.noreply.github.com>

--- ZakatTani/src/zakattani/cetak_Hasil.java
+++ ZakatTani/src/zakattani/cetak_Hasil.java
@@ -27,21 +27,26 @@ public class cetak_Hasil extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel4 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         tf_hasilPanen = new javax.swing.JTextField();
         tf_harga = new javax.swing.JTextField();
+        cb_biaya = new javax.swing.JComboBox<>();
         btn_hitung = new javax.swing.JToggleButton();
         lb_hasil = new javax.swing.JLabel();
 
+        jLabel4.setText("jLabel4");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Untung Panen");
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("ZakatTani");
 
         jLabel2.setText("Hasil Panen");
 
-        jLabel3.setText("harga per/kg");
+        jLabel3.setText("Harga Panen");
 
         tf_hasilPanen.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -55,12 +60,26 @@ public class cetak_Hasil extends javax.swing.JFrame {
             }
         });
 
-        btn_hitung.setText("Hitung Untung");
+        cb_biaya.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        cb_biaya.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "--->Pilih Biaya Perawatan<---", "Ada biaya perawatan", "Tidak ada biaya perwatan" }));
+        cb_biaya.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        cb_biaya.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cb_biayaActionPerformed(evt);
+            }
+        });
+
+        btn_hitung.setText("Hitung Zakat");
+        btn_hitung.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btn_hitung.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btn_hitungActionPerformed(evt);
             }
         });
+
+        lb_hasil.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lb_hasil.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lb_hasil.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -71,41 +90,47 @@ public class cetak_Hasil extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel2)
                     .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(tf_hasilPanen, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(tf_harga, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(tf_hasilPanen, javax.swing.GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+                    .addComponent(tf_harga))
                 .addGap(66, 66, 66))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(155, 155, 155)
-                        .addComponent(jLabel1))
+                        .addGap(172, 172, 172)
+                        .addComponent(btn_hitung))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(141, 141, 141)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(btn_hitung, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(lb_hasil, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(178, 178, 178)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(lb_hasil, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addGap(149, 149, 149)
+                            .addComponent(cb_biaya, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(178, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(tf_hasilPanen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(53, 53, 53)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(tf_harga, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(17, 17, 17)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(tf_hasilPanen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(tf_harga, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(30, 30, 30)
+                .addComponent(cb_biaya, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(51, 51, 51)
                 .addComponent(btn_hitung)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 70, Short.MAX_VALUE)
-                .addComponent(lb_hasil)
-                .addGap(53, 53, 53))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 81, Short.MAX_VALUE)
+                .addComponent(lb_hasil, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(52, 52, 52))
         );
 
         pack();
@@ -120,14 +145,30 @@ public class cetak_Hasil extends javax.swing.JFrame {
         double panen = Double.valueOf(tf_hasilPanen.getText().trim());
         double harga = Double.valueOf(tf_harga.getText().trim());
         
-        double untung = panen * harga;
+        String biaya = (String)cb_biaya.getSelectedItem();
         
-        lb_hasil.setText("Rp."+String.valueOf(untung));
+//        double untung = panen * harga;
+//        
+//        lb_hasil.setText("Rp."+String.valueOf(untung));
+        
+        if(panen < 653)
+            lb_hasil.setText("tidak memenuhi Syarat");
+        else if("--->Pilih Biaya Perawatan<---".equals(biaya))
+            lb_hasil.setText("Pilih biaya Perawatan!");
+        else
+            if("Ada biaya perawatan".equals(biaya))
+                lb_hasil.setText("Rp."+String.valueOf(panen*harga*0.05));
+            else if("Tidak ada biaya perawatan".equals(biaya))
+                lb_hasil.setText("Rp."+String.valueOf(panen*harga*0.010));
     }//GEN-LAST:event_btn_hitungActionPerformed
 
     private void tf_hargaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tf_hargaActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_tf_hargaActionPerformed
+
+    private void cb_biayaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cb_biayaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cb_biayaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -167,9 +208,11 @@ public class cetak_Hasil extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JToggleButton btn_hitung;
+    private javax.swing.JComboBox<String> cb_biaya;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel lb_hasil;
     private javax.swing.JTextField tf_harga;
     private javax.swing.JTextField tf_hasilPanen;
